23c3ed6762abc3f6a7ee44da7db4a8c6239e8628
jazee <jazeekyla@gmail.com>
2024-12-05 00:46:02 +0800

    changed

--- create_account.php
+++ create_account.php
@@ -77,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             color: #555;
             margin-bottom: 8px;
             display: block;
-            margin-left: -10px;
+            margin-left: -30px;
         }
 
         input[type="email"],
