cf9183db1192661cf81d8bde2d9cac8f737e9ae2
jazee <jazeekyla@gmail.com>
2024-12-19 21:25:54 +0800

    updatyed

--- timeline.php
+++ timeline.php
@@ -98,6 +98,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($user['email']) ?>'s Timeline</title>
     <link rel="stylesheet" href="css/timeline.css">
+
 </head>
 <body>
     <header>
